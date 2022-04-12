eac86996786d2427448f9300bd7f35e6d0ccfce6
Karen <yong-zhen.lai@epitech.eu>
2022-04-12 11:55:53 +0200

    add admin kpi

--- yowl/src/components/AdminComponents/AdminData.vue
+++ yowl/src/components/AdminComponents/AdminData.vue
@@ -5,7 +5,11 @@
     <br>
     communities in total : {{this.communities.length}}
     <br>
-    new users today : {{ this.filteredUsers.length }}
+    new users today : {{ this.NewUsersToday.length }}
+    <br>
+    average new users daily : {{ this.AVGUsers }}
+    <br>
+    average new posts daily : {{ this.AVGPosts }}
 </template>
 
 <script>
@@ -17,28 +21,44 @@ export default {
       users: [],
       posts: [],
       communities: [],
-      filteredUsers: []
+      NewUsersToday: [],
+      AVGUsers: 0,
+      AVGPosts: 0
     }
   },
   methods: {
     getCurrentDate () {
       const localTime = moment().format('YYYY-MM-DD')
       return localTime
+    },
+    daysSinceStart () {
+      const start = moment('2022-04-08', 'YYYY-MM-DD')
+      const current = moment().startOf('day')
+      const diffDays = moment.duration(current.diff(start)).asDays() + 1
+      return diffDays
+    },
+    averageUsersAndPosts () {
+      const totalUsers = this.users.length
+      const totalPosts = this.posts.length
+      const diffDays = this.daysSinceStart()
+      this.AVGUsers = totalUsers / diffDays
+      this.AVGPosts = totalPosts / diffDays
     }
   },
   created () {
-    this.getNewUsersToday()
     axios.get('https://yowlteam.herokuapp.com/api/users')
       .then((response) => {
         console.log('users is', response.data)
         this.users = response.data
-        this.filteredUsers = this.users.filter((user) => user.created_at.substring(0, 10) === this.getCurrentDate())
+        this.NewUsersToday = this.users.filter((user) => user.created_at.substring(0, 10) === this.getCurrentDate())
+        this.averageUsersAndPosts()
       })
       .catch(error => console.log(error))
     axios.get('https://yowlteam.herokuapp.com/api/posts')
       .then((response) => {
         // console.log('posts is', response.data)
         this.posts = response.data
+        this.averageUsersAndPosts()
       })
       .catch(error => console.log(error))
     axios.get('https://yowlteam.herokuapp.com/api/communities')
