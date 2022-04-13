77cb665b3022e70ee8f6fa506bb7a9d1781601ca
Karen <yong-zhen.lai@epitech.eu>
2022-04-13 17:57:31 +0200

    add name of post and user instead of id

--- yowl/src/components/AdminComponents/AdminCommentsManager.vue
+++ yowl/src/components/AdminComponents/AdminCommentsManager.vue
@@ -16,6 +16,8 @@
         :key='user.id'
       > -->
         <AdminEachComment v-for='comment in comments' :key='comment.id' :comment="comment"
+        @deleteComment='deleteComment'
+        @passComment='passComment'
         ></AdminEachComment>
       <!-- </div> -->
     </tbody>
@@ -37,6 +39,24 @@ export default {
     AdminNavButtons
   },
   methods: {
+    deleteComment (commentId) {
+      axios.delete('https://yowlteam.herokuapp.com/api/comments/' + commentId)
+        .then((response) => {
+          this.comments = [...this.comments.filter((element) => element.id !== commentId)]
+        })
+    },
+    passComment (commentId) {
+      // console.log('in delete post function ', postId)
+      axios.put('https://yowlteam.herokuapp.com/api/comments/' + commentId,
+        {
+          is_reported: 0
+        })
+        .then((response) => {
+          this.comments = [...this.comments.filter((element) => element.id !== commentId)]
+          // console.log('AFTERRRRRR posts is', this.posts)
+        })
+        .catch(error => console.log(error))
+    }
   },
   created () {
     axios.get('https://yowlteam.herokuapp.com/api/comments')
--- yowl/src/components/AdminComponents/AdminEachComment.vue
+++ yowl/src/components/AdminComponents/AdminEachComment.vue
@@ -1,18 +1,19 @@
 <template>
   <tr>
     <td>{{commentId}}</td>
-    <td>{{commentContent}}</td>
     <td>{{commentUserId}}</td>
+    <td>{{commentContent}}</td>
     <td>{{commentPostId}} </td>
     <td>
-        <button class='button'>Delete</button>
-        <button class='button'>Pass</button>
+        <button class='button' @click="$emit('deleteComment', commentId)">Delete</button>
+        <button class='button' @click="$emit('passComment', commentId)">Pass</button>
     </td>
   </tr>
     <!-- <AdminPostModal></AdminPostModal> -->
 </template>
 
 <script>
+import axios from 'axios'
 // import AdminPostModal from './AdminPostModal.vue'
 export default {
   props: ['comment'],
@@ -21,13 +22,41 @@ export default {
       commentId: this.comment.id,
       commentContent: this.comment.content,
       commentUserId: this.comment.user_id,
-      commentPostId: this.comment.post_id
+      commentPostId: this.comment.post_id,
+      users: [],
+      posts: []
     }
   },
   // components: {
   //   AdminPostModal
   // }
   methods: {
+    getUserName (userId) {
+      // console.log('this user id is ', userId)
+      axios.get('https://yowlteam.herokuapp.com/api/users')
+        .then((response) => {
+          this.users = response.data
+          const user = this.users.filter((user) => user.id === userId)
+          const userName = user[0].pseudo
+          this.commentUserId = userName
+        })
+        .catch(error => console.log(error))
+    },
+    getPostTitle (postId) {
+      // console.log('this user id is ', userId)
+      axios.get('https://yowlteam.herokuapp.com/api/posts')
+        .then((response) => {
+          this.posts = response.data
+          const post = this.posts.filter((post) => post.id === postId)
+          const postTitle = post[0].title
+          this.commentPostId = postTitle
+        })
+        .catch(error => console.log(error))
+    }
+  },
+  created () {
+    this.getUserName(this.commentUserId)
+    this.getPostTitle(this.commentPostId)
   }
 }
 </script>
