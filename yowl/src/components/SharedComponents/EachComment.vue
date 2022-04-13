8981d8f18cbdc5eb5a6128f4e92e5aa1e5325578
michaelgarcia75 <michael.garcia@epitech.eu>
2022-04-13 11:54:47 +0200

    commentmanager eachcomment display comment almost finish

--- yowl/src/components/SharedComponents/EachComment.vue
+++ yowl/src/components/SharedComponents/EachComment.vue
@@ -1,3 +1,27 @@
 <template>
-    I'm each Comment
-    </template>
+  <div class="eachComment">
+    <h1>{{ comment.user_id }}</h1>
+    <br />
+    {{ comment.content }}
+    <br />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  props: ['comment'],
+  name: 'IndexView',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.eachComment {
+  background-color: antiquewhite;
+  padding: 10px;
+  margin: 20px;
+}
+</style>
--- yowl/src/components/SharedComponents/EachPost.vue
+++ yowl/src/components/SharedComponents/EachPost.vue
@@ -6,18 +6,36 @@
     <br />
     <br />
     <VoteButtons :post="post" />
+    <CommentsManager :commentsFiltered="commentsFiltered"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import VoteButtons from '@/components/SharedComponents/VoteButtons.vue'
+import axios from 'axios'
+import CommentsManager from '@/components/SharedComponents/CommentsManager.vue'
 
 export default {
   props: ['post'],
   name: 'IndexView',
   components: {
-    VoteButtons
+    VoteButtons,
+    CommentsManager
+  },
+  data () {
+    return {
+      commentsFiltered: []
+    }
+  },
+  created () {
+    axios.get('https://yowlteam.herokuapp.com/api/comments/post/' + this.post.id)
+      .then((response) => {
+        // console.log('comments is', response.data)
+        this.commentsFiltered = response.data
+        // console.log('comments filtered is', this.commentsFiltered)
+      })
+      .catch(error => console.log(error))
   }
 }
 </script>
