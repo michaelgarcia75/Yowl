8f0c353c12c0c3ed59939fe8b91bffe02b3fe697
Florent Dizes <florent.dizes@epitech.eu>
2022-04-14 09:50:23 +0200

    user-profile

--- yowl/src/components/SpecialComponents/UserSettings.vue
+++ yowl/src/components/SpecialComponents/UserSettings.vue
@@ -1,100 +1,118 @@
 <template>
-<div class="userSetting">
-<div class="settingTitle">
-    <h1>Account Settings</h1>
-    <button class="editButton" v-if="isOpen" @click="isOn=true, isOpen=false">Edit</button>
-    <button class="returnButton" v-if="isOn" @click="isOn=false, isOpen=true">Return</button>
+  <div class="userSetting">
+    <div class="settingTitle">
+      <h1>Account Settings</h1>
+      <button class="editButton" v-if="isOpen" @click="isOn=true, isOpen=false">Edit</button>
+      <button class="returnButton" v-if="isOn" @click="isOn=false, isOpen=true">Return</button>
     </div>
-<div class="settingContent" v-if="isOpen">
-    <img id="userImage" src="./hamzabg.jpg" />
-    <div class="userSettingInfo">
+    <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="(error,index) in errors" :key="index">{{ error }}</li>
+    </ul>
+  </p>
+    <div class="settingContent" v-if="isOpen">
+      <img id="userImage" src="./hamzabg.jpg" />
+      <div class="userSettingInfo">
         <h1>Username</h1>
-         <p>{{ users[0]?.pseudo }}</p>
-         <br>
-        <h1>Email Adress</h1>
-         <p>{{ users[0]?.email }}</p>
-         <br>
-        <h1>Password</h1>
-        <p>********</p>
-    </div>
-</div>
-<div v-if="isOn"  class="settingContent">
-    <img id="userImage" src="./hamzabg.jpg" />
-    <div class="userSettingInfo">
-        <h1>Username</h1>
-         <input type="text" id="pseudo" v-model="newPseudo" :placeholder="users[0]?.pseudo" >
-        <h1>Email Adress</h1>
-         <input type="email" id="email" v-model="newEmail" :placeholder="users[0]?.email">
-        <h1>Password</h1>
-        <input type="password" id="password" v-model="newPassword" placeholder="********">
-         <h1>Confirmation</h1>
-        <input type="password" id="confirmation" v-model="confirmation" placeholder="********">
+        <p>{{ newPseudo }}</p>
         <br>
-        <button class="submitButton" @click="editUser(users[0].id, newPseudo, newEmail, newPassword, confirmation), isOn=false, isOpen=true">Submit</button>
+        <h1>Email Adress</h1>
+        <p>{{ newEmail }}</p>
+        <br>
+        <!-- <h1>Password</h1>
+        <p>********</p> -->
+      </div>
     </div>
-</div>
-</div>
+    <div v-if="isOn"  class="settingContent">
+      <img id="userImage" src="./hamzabg.jpg" />
+      <div class="userSettingInfo">
+        <h1>Username</h1>
+        <input type="text" id="pseudo" v-model="newPseudo" placeholder="Michael le gros tocard" >
+        <h1>Email Adress</h1>
+        <input type="email" id="email" v-model="newEmail" placeholder="michael@toc.ard">
+        <h1>Password</h1>
+        <input type="password" id="password" v-model="newPassword">
+        <h1>Confirmation</h1>
+        <input type="password" id="confirmation" v-model="confirmation">
+        <br>
+        <button class="submitButton" @click="editUser(user.id, newPseudo, newEmail, newPassword, confirmation)">Submit</button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import axios from 'axios'
-
-const isOn = ref(false)
-const isOpen = ref(true)
-
-</script>
-
 <script>
+import axios from 'axios'
+import { mapGetters, mapMutations } from 'vuex'
 const character = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
 export default {
   data () {
     return {
-      users: [],
+      isOn: false,
+      isOpen: true,
+      errors: [],
+      user: {},
       pseudo: '',
-      email: ''
+      email: '',
+      newPseudo: '',
+      newEmail: '',
+      newPassword: '',
+      confirmation: ''
     }
   },
+  computed: {
+    ...mapGetters(['isLoggedIn', 'getToken', 'getUser'])
+  },
   methods: {
+    ...mapMutations(['setUser', 'setToken']),
     editUser (userId, newPseudo, newEmail, newPassword, confirmation) {
       if (newPseudo === undefined && newPassword === undefined) {
-        alert('Fields Empty')
-        this.errors.push()
+        this.errors.push('Fields Empty')
       }
       if (newPseudo === undefined || newPseudo.length === 0) {
-        alert('Username Field Empty')
-        this.errors.push()
+        this.errors.push('Username Field Empty')
       }
       if (newPassword === undefined || newPassword.length === 0) {
-        alert('Password Field Empty')
-        this.errors.push()
+        this.errors.push('Password Field Empty')
       }
-      if (newPseudo.length < 8 && newPseudo.length > 0) {
-        alert('Username Minimum 8 characters.')
-        this.errors.push()
+      if (newPseudo.length < 2 && newPseudo.length > 0) {
+        this.errors.push('Username Minimum 8 characters')
       }
       if (!character.test(newEmail)) {
-        alert('Please enter a valid email address')
-        this.errors.push()
+        this.errors.push('Please enter a valid email address')
       }
       if (newPassword !== confirmation) {
-        alert('Password and Confirmation are not identical')
-        this.errors.push()
+        this.errors.push('Password and Confirmation are not identical')
       }
-      axios.put('https://yowlteam.herokuapp.com/api/users/' + userId, {
-        pseudo: newPseudo,
-        email: newEmail
-      })
-      this.users[0].pseudo = this.newPseudo
-      this.users[0].email = this.newEmail
+      if (!this.errors.length) {
+        axios.put('https://yowlteam.herokuapp.com/api/users/' + userId, {
+          pseudo: newPseudo,
+          email: newEmail,
+          password: newPassword
+        })
+        this.user.pseudo = newPseudo
+        this.user.email = newEmail
+        this.isOn = false
+        this.isOpen = true
+      }
     }
   },
   beforeCreate () {
-    axios.get('https://yowlteam.herokuapp.com/api/users')
-      .then((response) => {
-        this.users = response.data
-      })
-      .catch()
+    // axios.get('https://yowlteam.herokuapp.com/api/users')
+    //   .then((response) => {
+    //     console.log(response.data)
+    //     this.user = response.data
+    //     this.newPseudo = this.user[0].pseudo
+    //     this.newEmail = this.user[0].email
+    //   })
+    //   .catch()
+  },
+  created () {
+    this.user = this.getUser
+    console.log(this.user)
+    this.newPseudo = this.user.pseudo
+    this.newEmail = this.user.email
   }
 }
 </script>
