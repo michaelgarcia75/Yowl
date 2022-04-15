e7ade74eb01af55a39e9ae6dad9da93c6ad80565
Ines <ines.giraud-carrier@epitech.eu>
2022-04-15 09:38:55 +0200

    rm createComunityModal

--- yowl/src/App.vue
+++ yowl/src/App.vue
@@ -5,7 +5,7 @@
     </router-link>
     <SearchBar @getPostBySearch="getPostBySearch"/>
     <CategoriesFilter :filterAll="filterAll" :categories="categories" @getPostByCategoryFilter="getPostByCategoryFilter"/>
-    <router-link v-if="isLoggedIn && getUser && user.is_admin === 1" to="/admin">Admin</router-link>
+    <router-link  to="/admin">Admin</router-link>
     <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
     <router-link v-if="!isLoggedIn" to="/register">Register</router-link>
     <DropDownMenu menu-title="Menu">
@@ -15,8 +15,7 @@
       <section v-if="isLoggedIn" class="option">
         <router-link to="/dashboard">Profil</router-link>
       </section>
-      <section class="option">
-      <CommunityButton/>
+      <section v-if="isLoggedIn" class="option">
       </section>
       <section v-if="isLoggedIn" class="option">
         <a @click="logout(), user={}" >Log Out</a>
@@ -25,6 +24,7 @@
         <span class="desc">Dark mode // OPTION</span>
       </section>
     </DropDownMenu>
+
   </nav>
   <router-view :postsFiltered="postsFiltered" :searchContent="searchContent"/>
 </template>
@@ -34,7 +34,6 @@ import axios from 'axios'
 import SearchBar from '@/components/SharedComponents/SearchBar.vue'
 import CategoriesFilter from '@/components/SharedComponents/CategoriesFilter.vue'
 import DropDownMenu from '@/components/SharedComponents/DropDownMenu.vue'
-import CommunityButton from '@/components/SharedComponents/CommunityButton.vue'
 import { mapGetters, mapMutations } from 'vuex'
 
 export default {
@@ -56,8 +55,7 @@ export default {
   components: {
     SearchBar,
     CategoriesFilter,
-    DropDownMenu,
-    CommunityButton
+    DropDownMenu
   },
   computed: {
     ...mapGetters(['isLoggedIn', 'getToken', 'getUser'])
@@ -65,35 +63,23 @@ export default {
   methods: {
     ...mapMutations(['setUser', 'setToken']),
     getPostBySearch (searchContent) {
-      console.log('A')
       this.searchContent = searchContent
       if (this.categoryId === 0) {
-        console.log('A1')
-
-        // console.log('POST BY SEARCH 1', searchContent)
         axios.get('https://yowlteam.herokuapp.com/api/posts/filter?text=' + searchContent)
           .then((response) => {
             this.searchResult = [...response.data]
             this.postsFiltered = [...response.data]
-            // console.log('my searchResult is', response.data)
           })
       } else {
-        console.log('A2')
-        // console.log('POST BY SEARCH 2 "', searchContent, '" AND', this.categoryId)
         axios.get('https://yowlteam.herokuapp.com/api/posts/filter?text=' + searchContent + '&category=' + this.categoryId)
           .then((response) => {
             this.postsFiltered = [...response.data]
           })
       }
     },
-    // /!\ situation when you enter searchContent and filterContent at the same time is missing !!!
     getPostByCategoryFilter (categoryId = null) {
-      console.log('B')
       if (this.searchContent === '') {
-        console.log('B1')
         if (categoryId === null) {
-          console.log('B1a')
-          // console.log('POST BY FILTER ALL')
           axios.get('https://yowlteam.herokuapp.com/api/posts')
             .then((response) => {
               this.posts = response.data
@@ -101,51 +87,25 @@ export default {
             })
             .catch(error => console.log(error))
         } else {
-          console.log('B1b')
-          // console.log('I am in get post by filter categoryId is ', this.searchResult)
           axios.get('https://yowlteam.herokuapp.com/api/posts/filter?category=' + categoryId)
             .then((response) => {
               this.categoryId = categoryId
               this.postsFiltered = [...response.data]
-              // console.log('my category name is', categoryId)
             })
         }
       } else {
-        console.log('B2')
         if (categoryId === null) {
-          console.log('B2a')
           this.getPostBySearch(this.searchContent)
         } else {
-          console.log('B2b')
           axios.get('https://yowlteam.herokuapp.com/api/posts/filter?text=' + this.searchContent + '&category=' + categoryId)
             .then((response) => {
               this.postsFiltered = [...response.data]
-              console.log(response.data)
             })
         }
       }
     },
     getPostBySearchAndCategoryFilter (searchContent, categoryId) {
       axios.get('https://yowlteam.herokuapp.com/api/posts/filter?text=' + searchContent)
-      //   .then((response) => {
-      //     this.searchResult = [...response.data]
-      //   })
-      // console.log('I am in get pot by search AND filter', searchContent, categoryId)
-      // axios.get('https://yowlteam.herokuapp.com/api/posts/filter?' + searchContent)
-      //   .then((response) => {
-      //     this.searchResult = [...response.data]
-      //   })
-      // axios.get('https://yowlteam.herokuapp.com/api/posts/filter?' + categoryId)
-      //   .then((response) => {
-      //     this.filterResult = [...response.data]
-      //   })
-      // for (let i = 0; i < this.searchResult.length; i++) {
-      //   for (let j = 0; j < this.filterResult.length; j++) {
-      //     if (this.searchResult[i] === this.filterResult[j]) {
-      //       this.searchAndFilterResult.push(this.searchResult[i])
-      //     }
-      //   }
-      // }
     },
     getUserLogin () {
       this.user = this.getUser
@@ -158,55 +118,17 @@ export default {
       instance.defaults.headers.common.Authorization = `Bearer ${this.getToken}`
       instance.post('/auth/logout')
         .then((response) => {
-          console.log(response)
           this.setToken('')
           this.setUser({})
           localStorage.clear()
           this.$router.push('/')
         })
         .catch((error) => console.log(error))
-      // const config = {
-      //   headers: {
-      //     Authorization: `Bearer ${this.getToken}`
-      //   }
-      // }
-      // axios.post('https://yowlteam.herokuapp.com/api/auth/logout', config)
-      //   .then((response) => {
-      //     console.log(response)
-      //     this.setToken('')
-      //     this.setUser({})
-      //     sessionStorage.clear()
-      //     this.$router.push('/')
-      //   })
-      //   .catch((error) => console.log(error))
     }
-    // getPostBySearchAndCategoryFilter (searchContent, categoryId) {
-    //   axios.get('https://yowlteam.herokuapp.com/api/posts/filter?text=' + searchContent)
-    //     .then((response) => {
-    //       this.searchResult = [...response.data]
-    //     })
-    //   console.log('I am in get pot by search AND filter', searchContent, categoryId)
-    //   axios.get('https://yowlteam.herokuapp.com/api/posts/filter?' + searchContent)
-    //     .then((response) => {
-    //       this.searchResult = [...response.data]
-    //     })
-    //   axios.get('https://yowlteam.herokuapp.com/api/posts/filter?' + categoryId)
-    //     .then((response) => {
-    //       this.filterResult = [...response.data]
-    //     })
-    //   for (let i = 0; i < this.searchResult.length; i++) {
-    //     for (let j = 0; j < this.filterResult.length; j++) {
-    //       if (this.searchResult[i] === this.filterResult[j]) {
-    //         this.searchAndFilterResult.push(this.searchResult[i])
-    //       }
-    //     }
-    //   }
-    // }
   },
   created () {
     this.getPostBySearch()
     this.user = this.getUser
-    console.log('admin is', this.user.is_admin)
     axios.get('https://yowlteam.herokuapp.com/api/posts')
       .then((response) => {
         this.posts = response.data
